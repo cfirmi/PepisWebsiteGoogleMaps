0e206a0a968d31c444f057a954f49db420f205ea
practice <practice@CPE003ee1c8f5f2-CM00fc8d41fe90.cpe.net.cable.rogers.com>
2017-08-31 15:52:25 -0400

    init commit

---
+++ src/js/navbar.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.fa-bars').click(function() {
+        $('.sidebar-nav-closed').toggleClass('sidebar-nav-opened')
+        $('.close-rect-hidden').toggleClass('close-rect-showing')
+    });
+    $('.close-rect-hidden').click(function() {
+        $('.sidebar-nav-closed').toggleClass('sidebar-nav-opened')
+        $('.close-rect-hidden').toggleClass('close-rect-showing')
+    });
+      $('.fa-bars').click(function() {
+    $('.pepisLogo').toggleClass('pepisLogo-nav')
+    });
+    $('.close-rect-hidden').click(function() {
+    $('.pepisLogo').toggleClass('pepisLogo-nav')
+  });
+});
